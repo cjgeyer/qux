ef2ffe739ee16777ea2dc930d69f53414c561f88
Charles J. Geyer <geyer@umn.edu>
2024-02-18 08:20:39 -0600

    fixed again, now ver 0.6-1
    use R function .onLoad to initialize C static variables to
    R_NilValue which C does not consider to be a constant

--- package/qux/src/quc.c
+++ package/qux/src/quc.c
@@ -3,8 +3,18 @@
 #include <Rinternals.h>
 #include <stdarg.h>
 
-static SEXP thefunction = NULL;
-static SEXP theenvironment = NULL;
+static SEXP thefunction;
+static SEXP theenvironment;
+
+// this is called from R function .onLoad at package load time
+// and is also called after each evaluation of the function
+// in C function F77_SUB(doit) below
+// We cannot use static initialization because R_NilValue is not a constant
+// as far as C is concerned
+void initialize(void) {
+    thefunction = R_NilValue;
+    theenvironment = R_NilValue;
+}
 
 SEXP setupfunction(SEXP func, SEXP rho)
 {
@@ -17,17 +27,11 @@ SEXP setupfunction(SEXP func, SEXP rho)
     return(ScalarLogical(TRUE));
 }
 
-static void teardownfunction()
-{
-    thefunction = R_NilValue;
-    theenvironment = R_NilValue;
-}
-
 double F77_SUB(doit)(double *x, int *n)
 {
-    if ((! thefunction) || (! isFunction(thefunction)))
+    if (! isFunction(thefunction))
         error("setupfunction needs to be called first");
-    if ((! theenvironment) || (! isEnvironment(theenvironment)))
+    if (! isEnvironment(theenvironment))
         error("setupfunction needs to be called first");
 
     SEXP state;
@@ -38,7 +42,7 @@ double F77_SUB(doit)(double *x, int *n)
     SEXP call, result;
     PROTECT(call = lang2(thefunction, state));
     PROTECT(result = eval(call, theenvironment));
-    teardownfunction();
+    initialize();
 
     if (! isVectorAtomic(result))
         error("result of function call must be atomic");
